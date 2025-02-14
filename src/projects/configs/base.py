4b196cdf9984f0405d675f2ea2ec0c0a7cf61dac
Qwelice <Qwelice@yandex.ru>
2025-02-14 14:30:15 +0300

    config_name fix

--- src/projects/configs/base.py
+++ src/projects/configs/base.py
@@ -4,7 +4,7 @@ from src.utils.configuration import Configuration
 
 
 def get_configuration(config_name: str) -> Configuration:
-    config_name = os.path.join(__file__, f'{config_name}.yaml')
+    config_name = os.path.join(os.path.dirname(__file__), f'{config_name}.yaml')
     config = Configuration.load_cfg(config_name)
     return config
 

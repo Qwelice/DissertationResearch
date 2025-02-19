generator and discriminator, bug fixes, normalization addition

projects/AVoT/layers/synthesis.py
@@ -5,6 +5,7 @@ from torch import nn
 
 from . import conv as cnv
 from . import attention as atten
+from .attention import VoxAttentionL2
 
 
 class VoxDecoderBlock(nn.Module):
@@ -54,12 +55,43 @@ class VoxDecoderBlock(nn.Module):
 
 
 class VoxelizingBlock(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, activation: str='sigmoid'):
         super(VoxelizingBlock, self).__init__()
         self.conv = nn.Conv3d(in_channels=in_channels, out_channels=1, kernel_size=1, stride=1)
+        if activation == 'sigmoid':
+            self.activation = nn.Sigmoid()
+        else:
+            self.activation = nn.Tanh()
 
     def forward(self, voxel):
-        return self.conv(voxel)
+        raw = self.conv(voxel)
+        voxel = self.activation(raw)
+        return voxel
+
+
+class DevoxelizingBlock(nn.Module):
+    def __init__(self, out_channels: int, num_groups: int=32):
+        super(DevoxelizingBlock, self).__init__()
+        self.conv_1 = nn.Conv3d(in_channels=1,
+                                out_channels=out_channels,
+                                kernel_size=3,
+                                stride=1,
+                                padding=1)
+        self.conv_2 = nn.Conv3d(in_channels=out_channels,
+                                out_channels=out_channels,
+                                kernel_size=3,
+                                stride=1,
+                                padding=1)
+        self.num_groups = math.gcd(out_channels, num_groups)
+        self.norm = nn.GroupNorm(self.num_groups, out_channels)
+        self.activation = nn.LeakyReLU(0.2)
+
+    def forward(self, x):
+        out = self.conv_1(x)
+        out = self.norm(out)
+        out = self.activation(out)
+        out = self.conv_2(out)
+        return out
 
 
 class SingleDecoderLayer(nn.Module):
@@ -84,11 +116,83 @@ class SingleDecoderLayer(nn.Module):
         self._upsample = upsample
 
     def forward(self, x, style):
-        x = self.conv(x, style)
         if self._upsample:
             x = nn.functional.interpolate(x, scale_factor=2, mode='trilinear')
+        x = self.conv(x, style)
         out = self.voxelizing(x)
         return x, out
+
+
+class SingleEncoderLayer(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 num_groups: int=32,
+                 downsample: bool=True):
+        super(SingleEncoderLayer, self).__init__()
+        self.id_conv = nn.Conv3d(in_channels=in_channels,
+                                 out_channels=out_channels,
+                                 kernel_size=3,
+                                 stride=1,
+                                 padding=1)
+        self.downsample = nn.Conv3d(in_channels=out_channels,
+                                    out_channels=out_channels,
+                                    kernel_size=3,
+                                    stride=2 if downsample else 1,
+                                    padding=1)
+
+        self.num_groups = math.gcd(out_channels, num_groups)
+        self.norm = nn.GroupNorm(self.num_groups, out_channels)
+
+        self.activation = nn.LeakyReLU(0.2)
+
+        self.voxelizing = VoxelizingBlock(out_channels)
+
+    def forward(self, x):
+        x = self.id_conv(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        x = self.downsample(x)
+        vox = self.voxelizing(x)
+        return x, vox
+
+
+class VoxEncoderLayer(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 voxel_size_after: int,
+                 patch_size_after: int,
+                 num_groups: int=32,
+                 attn_head: int=4,
+                 attn_emb_dim: int=256,
+                 attn_tie_qk: bool=True):
+        super(VoxEncoderLayer, self).__init__()
+        self.id_conv = nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.downsample = nn.Conv3d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=2,
+                                    padding=1)
+        self.num_groups = math.gcd(out_channels, num_groups)
+        self.norm = nn.GroupNorm(self.num_groups, out_channels)
+        self.activation = nn.LeakyReLU(0.2)
+
+        self.attn = VoxAttentionL2(in_channels=out_channels,
+                                   nhead=attn_head,
+                                   voxel_size=voxel_size_after,
+                                   patch_size=patch_size_after,
+                                   emb_dim=attn_emb_dim,
+                                   tie_qk=attn_tie_qk)
+        self.voxelizing = VoxelizingBlock(in_channels=out_channels)
+
+    def forward(self, x):
+        x = self.id_conv(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        x = self.downsample(x)
+        bs, c, d, w, h = x.shape
+        out = self.attn(x)
+        out = out.view(bs, c, d, w, h)
+        vox = self.voxelizing(out)
+        return out, vox
 
 
 class VoxDecoderLayer(nn.Module):

projects/AVoT/modeling.py
@@ -4,14 +4,14 @@ import torch
 from torch import nn
 import torchvision.models as tv_models
 
-from layers.synthesis import SingleDecoderLayer
-from layers.synthesis import VoxDecoderLayer
+from layers.synthesis import SingleDecoderLayer, VoxDecoderLayer, SingleEncoderLayer, VoxEncoderLayer, DevoxelizingBlock
 from layers.attention import FeaturesAttentionL2
+from layers.conv import AdaptiveModulatedConv3d
 from src.utils.configuration import Configuration
 
 
 class SynthesisNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, mod_dim: int, descriptor_dim: int):
         super(SynthesisNetwork, self).__init__()
         self.image_tensor = nn.Parameter(torch.zeros(1, 512, 4, 4, 4, dtype=torch.float32))
         self.input_layer = SingleDecoderLayer(in_channels=512,
@@ -19,15 +19,15 @@ class SynthesisNetwork(nn.Module):
                                               kernel_size=3,
                                               stride=1,
                                               padding=1,
-                                              mod_dim=94,
+                                              mod_dim=mod_dim,
                                               bank_size=4)
 
         self.upsampling_1 = VoxDecoderLayer(in_channels=256,
                                             out_channels=128,
                                             voxel_size=4,
                                             patch_size=1,
-                                            descriptor_size=256,
-                                            mod_dim=94,
+                                            descriptor_size=descriptor_dim,
+                                            mod_dim=mod_dim,
                                             bank_size=4,
                                             attn_nhead=4,
                                             attn_emb_dim=512,
@@ -37,8 +37,8 @@ class SynthesisNetwork(nn.Module):
                                             out_channels=64,
                                             voxel_size=8,
                                             patch_size=1,
-                                            descriptor_size=256,
-                                            mod_dim=94,
+                                            descriptor_size=descriptor_dim,
+                                            mod_dim=mod_dim,
                                             bank_size=4,
                                             attn_emb_dim=512,
                                             xattn_emb_dim=512) # (8, 8, 8) -> (16, 16, 16)
@@ -48,7 +48,7 @@ class SynthesisNetwork(nn.Module):
                                                kernel_size=3,
                                                stride=1,
                                                padding=1,
-                                               mod_dim=94,
+                                               mod_dim=mod_dim,
                                                bank_size=4,
                                                upsample=True) # (16, 16, 16) -> (32, 32, 32)
 
@@ -95,6 +95,7 @@ class MappingNetwork(nn.Module):
     def __init__(self, latent_dim: int, descriptor_dim: int):
         super(MappingNetwork, self).__init__()
         self._latent_dim = latent_dim
+        self._negative_slope = 0.2
         self.fc_1 = nn.Linear(latent_dim + descriptor_dim, 32)
         self.fc_2 = nn.Linear(32, 64)
         self.fc_3 = nn.Linear(64, 128)
@@ -104,15 +105,161 @@ class MappingNetwork(nn.Module):
 
     def forward(self, x):
         device = x.device
-        neg_slope = 0.2
-        x = x.unsqueeze(1)
+        if x.ndim == 3:
+            x = x.squeeze(1)
         bs, _ = x.shape
         latent = torch.randn(bs, self._latent_dim, device=device)
         out = torch.cat([x, latent], dim=1)
-        out = nn.functional.leaky_relu(self.fc_1(out), neg_slope)
-        out = nn.functional.leaky_relu(self.fc_2(out), neg_slope)
-        out = nn.functional.leaky_relu(self.fc_3(out), neg_slope)
-        out = nn.functional.leaky_relu(self.fc_4(out), neg_slope)
-        out = nn.functional.leaky_relu(self.fc_5(out), neg_slope)
-        out = nn.functional.leaky_relu(self.fc_6(out), neg_slope)
+        out = nn.functional.leaky_relu(self.fc_1(out), self._negative_slope)
+        out = nn.functional.leaky_relu(self.fc_2(out), self._negative_slope)
+        out = nn.functional.leaky_relu(self.fc_3(out), self._negative_slope)
+        out = nn.functional.leaky_relu(self.fc_4(out), self._negative_slope)
+        out = nn.functional.leaky_relu(self.fc_5(out), self._negative_slope)
+        out = nn.functional.leaky_relu(self.fc_6(out), self._negative_slope)
         return out
+
+
+class PredictorNetwork(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, voxel_size: int, mod_dim: int):
+        super(PredictorNetwork, self).__init__()
+        self.conv_1 = AdaptiveModulatedConv3d(in_channels=in_channels,
+                                              out_channels=out_channels,
+                                              kernel_size=1,
+                                              stride=1,
+                                              mod_dim=mod_dim,
+                                              bank_size=4)
+        self.conv_2 = AdaptiveModulatedConv3d(in_channels=out_channels,
+                                              out_channels=out_channels,
+                                              kernel_size=1,
+                                              stride=1,
+                                              mod_dim=mod_dim,
+                                              bank_size=4)
+        self.conv_3 = AdaptiveModulatedConv3d(in_channels=out_channels,
+                                              out_channels=out_channels,
+                                              kernel_size=1,
+                                              stride=1,
+                                              mod_dim=mod_dim,
+                                              bank_size=4)
+        self.conv_4 = AdaptiveModulatedConv3d(in_channels=out_channels,
+                                              out_channels=out_channels,
+                                              kernel_size=1,
+                                              stride=1,
+                                              mod_dim=mod_dim,
+                                              bank_size=4)
+        self.residual = nn.Conv3d(in_channels=out_channels,
+                                  out_channels=out_channels,
+                                  kernel_size=1,
+                                  stride=1)
+        self.classifier = nn.Linear(out_channels * voxel_size ** 3, 1)
+        self.conv_activation = nn.LeakyReLU(0.2)
+        self.cls_activation = nn.Sigmoid()
+
+    def forward(self, x, style):
+        x = self.conv_1(x, style)
+        x = self.conv_activation(x)
+        x = self.conv_2(x, style)
+        x = self.conv_activation(x)
+        x = self.conv_3(x, style)
+        x = self.conv_activation(x)
+        x = self.conv_4(x, style)
+        x = self.conv_activation(x)
+        x = x + self.residual(x)
+        out = torch.flatten(x, start_dim=1)
+        out = self.classifier(out)
+        out = self.cls_activation(out)
+        return out
+
+
+class DiscriminatorNet(nn.Module):
+    def __init__(self, descriptor_dim: int):
+        super(DiscriminatorNet, self).__init__()
+        self.devoxelizers = [
+            DevoxelizingBlock(out_channels=32),
+            DevoxelizingBlock(out_channels=64),
+            DevoxelizingBlock(out_channels=128)
+        ]
+        self.layers = [
+            SingleEncoderLayer(in_channels=32,
+                               out_channels=64,
+                               downsample=True),  # (32, 32, 32) -> (16, 16, 16)
+            VoxEncoderLayer(in_channels=64,
+                            out_channels=128,
+                            voxel_size_after=8,
+                            patch_size_after=1,
+                            attn_head=4,
+                            attn_emb_dim=512),  # (16, 16, 16) -> (8, 8, 8)
+            SingleEncoderLayer(in_channels=128,
+                               out_channels=256,
+                               downsample=True) # (8, 8, 8) -> (4, 4, 4)
+        ]
+        self.predictors = [
+            PredictorNetwork(in_channels=64,
+                             out_channels=32,
+                             voxel_size=16,
+                             mod_dim=descriptor_dim),
+            PredictorNetwork(in_channels=128,
+                             out_channels=64,
+                             voxel_size=8,
+                             mod_dim=descriptor_dim),
+            PredictorNetwork(in_channels=256,
+                             out_channels=128,
+                             voxel_size=4,
+                             mod_dim=descriptor_dim)
+        ]
+
+    def forward(self, x, style):
+        outs = []
+        if style.ndim == 3:
+            style = style.squeeze(1)
+        N = len(self.layers)
+        if len(x) - 1 != N:
+            raise ValueError('the number of inputs must be equal to the number of layers')
+        for i in range(N):
+            devox = self.devoxelizers[i]
+            inp = devox(x[i]) if devox is not None else x[i]
+            temp = self.layers[i](inp)
+            preds = [self.predictors[i](temp[0], style)]
+            out = (temp[1], preds)
+            if i + 1 <= N - 1:
+                for j in range(i + 1, N):
+                    temp = self.layers[j](temp[0])
+                    out[1].append(self.predictors[j](temp[0], style))
+            outs.append(out)
+
+        return outs
+
+
+def model_testing():
+    image = torch.empty(8, 3, 224, 224)
+    gen_ext = DescriptorFormer(nhead=4,
+                               input_dim=512,
+                               seq_size=49,
+                               output_dim=128,
+                               emb_dim=256)
+    dis_ext = DescriptorFormer(nhead=4,
+                               input_dim=512,
+                               seq_size=49,
+                               output_dim=128,
+                               emb_dim=256)
+    mapping = MappingNetwork(128, 128)
+    gen = SynthesisNetwork(128, 128)
+    dis = DiscriminatorNet(128)
+    des_gen = gen_ext(image)
+    des_dis = dis_ext(image)
+    gen_style = mapping(des_gen[1])
+    dis_style = des_dis[1]
+    voxels = gen(gen_style, des_gen[0])
+    vox_preds = dis(voxels, dis_style)
+    print('[voxels shapes]\n')
+    for i in range(len(voxels)):
+        print(f'[{i}]\t', voxels[i].shape)
+    print('\n[voxels and preds shapes]\n')
+    for i in range(len(vox_preds)):
+        item = vox_preds[i]
+        preds = item[1]
+        for j in range(len(preds)):
+            print(f'[{i},{j}]\t', item[0].shape, ', ', preds[j].shape)
+
+
+if __name__ == '__main__':
+    model_testing()
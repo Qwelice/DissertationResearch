a0240e95565a7ae9b9ce97f8d04e9df55264e421
Qwelice <Qwelice@yandex.ru>
2025-05-10 13:07:36 +0300

    [test io]

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,7 +6,4 @@ logger = setup_logger(__name__,)
 
 
 def test():
-    logger.info("starting io module tests.")
-    dirs = PathManager.listdir("data")
-    assert isinstance(dirs, List)
-    logger.info(f"extracted dirs: {dirs}")
+    logger.info(f"{PathManager.get_local_path()}")

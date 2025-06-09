18464bbe9d28d01db75828850b43763413782ea2
Qwelice <Qwelice@yandex.ru>
2025-06-09 10:58:07 +0300

    [visual logger]

--- src/research/config/experiment_config.py
+++ src/research/config/experiment_config.py
@@ -12,16 +12,29 @@ ROOT_DIR = root_dir()
 experiment_cfg = EasyDict()
 experiment_cfg.seed = 1488
 experiment_cfg.set_name = 'modelnet10'
-experiment_cfg.num_epochs = 20
+experiment_cfg.num_epochs = 10
 experiment_cfg.use_warmup = True
 experiment_cfg.warmup_steps = 500
-experiment_cfg.output_dir = os.path.join(ROOT_DIR, 'outputs')
+
+# Internal logs params
+experiment_cfg.logs = EasyDict()
+experiment_cfg.logs.min_level = ''
+
+experiment_cfg.logs.visual = EasyDict()
+experiment_cfg.logs.visual.tag = 'vis'
+experiment_cfg.logs.visual.voxel_tag = 'vis_vox'
+experiment_cfg.logs.visual.plot_tag = 'vis_plt'
+experiment_cfg.logs.visual.voxel_threshold = 0.5
+experiment_cfg.logs.visual.save_dir = os.path.join(ROOT_DIR, 'logs', 'internal', 'visual')
+
+experiment_cfg.logs.textual = EasyDict()
+experiment_cfg.logs.textual.tag = 'tex'
+experiment_cfg.logs.textual.save_dir = os.path.join(ROOT_DIR, 'logs', 'internal', 'runtime')
 
 # Training params
 experiment_cfg.train = EasyDict()
 experiment_cfg.train.batch_size = 32
 experiment_cfg.train.learning_rate = 1e-3
-experiment_cfg.train.continue_from_last = False
 experiment_cfg.train.shuffle = True
 experiment_cfg.train.num_workers = 3
 experiment_cfg.train.drop_last = True
@@ -69,7 +82,6 @@ experiment_cfg.discriminator.optimizer = {
 experiment_cfg.tensorflow = EasyDict()
 experiment_cfg.tensorflow.log_dir = os.path.join(ROOT_DIR, 'logs', 'api', 'tensorflow')
 experiment_cfg.tensorflow.experiment_name = 'GAMMA-modelnet10'
-experiment_cfg.tensorflow.version = '0.1.0'
 
 # Checkpoint params
 experiment_cfg.ckpt_dir = os.path.join(ROOT_DIR, 'models')
---
+++ src/research/utils/loggers/visual_logger.py
@@ -0,0 +1,63 @@
+import os
+from argparse import Namespace
+from datetime import datetime
+from typing import Union, Any, Optional, override, List
+
+from pytorch_lightning.loggers import Logger
+from pytorch_lightning.utilities import rank_zero_only
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+
+class VisualLogger(Logger):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self._experiment_id = datetime.now().strftime("%Y%m%d-%H%M%S")
+        self._voxel_tag = self.config.logs.visual.voxel_tag
+        self.threshold = self.config.logs.visual.voxel_threshold
+
+    def log_metrics(self, metrics: dict[str, float], step: Optional[int] = None) -> None:
+        pass
+
+    def log_hyperparams(self, params: Union[dict[str, Any], Namespace], *args: Any, **kwargs: Any) -> None:
+        pass
+
+    @property
+    def name(self) -> Optional[str]:
+        return self.config.logs.visual.tag
+
+    @property
+    def version(self) -> Optional[Union[int, str]]:
+        return self._experiment_id
+
+    @override
+    @property
+    def save_dir(self) -> Optional[str]:
+        save_dir = self.config.logs.visual.save_dir
+        os.makedirs(save_dir, exist_ok=True)
+        return save_dir
+
+    @rank_zero_only
+    def log_voxels(self, voxels: List[torch.Tensor], step: int):
+        for voxel in range(voxels):
+            for i in range(min(len(voxel), 4)):
+                if voxel.dim() == 5:
+                    voxel = voxel.squeeze(1)
+
+                voxel = voxel.detach().cpu().numpy()
+                v = voxels[i]
+                fig = plt.figure(figsize=(4, 4))
+                ax = fig.add_subplot(111, projection='3d')
+
+                filled = v > self.threshold
+                x, y, z = np.where(filled)
+
+                ax.scatter(x, y, z, c=z, cmap='viridis', s=10)
+                ax.set_axis_off()
+                ax.view_init(30, 120)
+
+                img_path = os.path.join(self.save_dir, f"{self._voxel_tag}_{step:06}_{i}.png")
+                plt.savefig(img_path)
+                plt.close(fig)
